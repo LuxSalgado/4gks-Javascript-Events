bfa2bc3c2fcce0d27c3eb004eb7e26193c923b89
LuxSalgado <lasalgado.14@est.ucab.edu.ve>
2021-08-05 20:32:48 +0000

    Ejercicios resueltos hasta el número 8

--- exercises/08-event-target/index.js
+++ exercises/08-event-target/index.js
@@ -1,6 +1,11 @@
 window.onload = function loadFn() {
 	var containerElm = document.getElementById("container");
 	containerElm.addEventListener("click", function(event) {
+		//Ese event se puede usar para saber la informacion del elemento que activo el evento
 		//you code here
+		alert(event.target.nodeName); //De esta manera
+		/* if (event.target.nodeName == "IMG") {
+			alert("ay papaaaaaa");
+		} */
 	});
 };
